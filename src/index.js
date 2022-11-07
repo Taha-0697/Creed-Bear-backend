d4666e3c6f10e81a9135b05e7aaec1d31dbdb131
Taha <taha.chaghtai.dev@gmail.com>
2022-11-07 04:18:52 +0400

    API ISSUES Resolved

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 const { faker } = require('@faker-js/faker');
 
 
-const PORT = process.env.NODE_APP_PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors())
